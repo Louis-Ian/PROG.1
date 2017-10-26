5a78fd0699dfbcc11ddae60b9e7c90053d36bf20
HMSS <heliowp11@gmail.com>
2017-10-26 12:51:41 -0300

    Add files via upload

---
+++ listaencadeada.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct registro{
+	char nome[101];
+	char tipo[2];
+	long long int matricula;
+	long long int telefone;
+	int ddd;
+	struct registro* prox;
+};
+typedef struct registro Registro;
+
+void inserirlista(struct registro **agenda,struct registro novo){
+	prox.novo=prox.(*agenda);
+	if (prox.)
+	prox.(*agenda)=novo;
+	
+}
